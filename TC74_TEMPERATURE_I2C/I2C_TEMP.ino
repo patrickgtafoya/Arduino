db3e7972c924a8fa4524fd0f91b2781cd7e20fa7
Patrick Tafoya <44828525+patrickgtafoya@users.noreply.github.com>
2019-11-08 14:23:25 -0700

    Add files via upload
    AVR C and AVR Assembly TWI operation of TC74 Thermometer

---
+++ TC74_TEMPERATURE_I2C/I2C_TEMP.ino
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include "I2C_TEMP.h"
+
+#define TC74ADDR 0x48
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  i2c_init();
+  i2c_start(TC74ADDR<<1);
+  i2c_write(0x00);
+  i2c_start((TC74ADDR<<1)+1);
+  uint8_t temperature = i2c_read();
+  i2c_stop();
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+  delay(1000);
+}
